79ae41abd8ccc09f8d3f198f37f558509b6472eb
TokeliImre <selcukelbas@gmail.com>
2025-06-05 22:45:33 +0300

    HashRouter ile yönlendirme düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import OyunPage from "./OyunPage";
 import HakkindaPage from "./HakkindaPage";
@@ -6,14 +6,14 @@ import Navbar from "./Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/oyun" element={<OyunPage />} />
         <Route path="/hakkinda" element={<HakkindaPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
